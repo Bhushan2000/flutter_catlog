b2adbee5b9b20cd60dce88968625d1839f9b548a
DESKTOP <waghdharebhushan71@gmail.com>
2021-06-16 15:24:27 -0700

    day2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final int days = 30;
+  final String name = "Bhushan";
+  bool isMale = true;
+  double pi = 3.142;
+
+  // num datatype accept both int and double datatype
+  num temperature = 30.5;
+
+  // compiler auto detect the datatype of it.
+  var day = "Wednesday";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catlog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days days of flutter by $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
