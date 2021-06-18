f38530e4ddd2e11474388366ecbb1c85a9ae6403
DESKTOP <waghdharebhushan71@gmail.com>
2021-06-17 22:49:16 -0700

    day18 completed

---
+++ lib/widgets/home_widgets/catalog_list.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_catlog/models/catlog.dart';
+import 'package:flutter_catlog/pages/home_details_page.dart';
+import 'package:flutter_catlog/widgets/home_widgets/catalog_image.dart';
+import 'package:flutter_catlog/widgets/themes.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class CatalogList extends StatelessWidget {
+  const CatalogList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      itemBuilder: (context, index) {
+        final catalog = CatlaogModel.items![index];
+
+        return InkWell(
+          child: CatalogItem(catalog: catalog),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => HomeDetailsPage(catalog: catalog),
+              ),
+            );
+          },
+        );
+      },
+      itemCount: CatlaogModel.items!.length,
+    );
+  }
+}
+
+class CatalogItem extends StatelessWidget {
+  final Item? catalog;
+  const CatalogItem({
+    Key? key,
+    @required this.catalog,
+  })  : assert(catalog != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return VxBox(
+        child: Row(
+      children: [
+        Hero(
+          tag: Key(catalog!.id.toString()),
+          child: CatalogImage(
+            image: catalog!.image.toString(),
+          ),
+        ),
+        Expanded(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            catalog!.name!.text.lg.color(MyTheme.darkBluishColour).bold.make(),
+            catalog!.desc!.text.make(),
+            10.heightBox, // sizebox
+            ButtonBar(
+              alignment: MainAxisAlignment.spaceBetween,
+              buttonPadding: Vx.mH8,
+              children: [
+                "\$${catalog!.price}".text.bold.xl.make(),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(MyTheme.darkBluishColour),
+                    shape: MaterialStateProperty.all(
+                      StadiumBorder(),
+                    ),
+                  ),
+                  child: "Buy".text.make(),
+                )
+              ],
+            ).pOnly(right: 8.0)
+          ],
+        ))
+      ],
+    )).white.rounded.square(150).make().py(16);
+  }
+}
