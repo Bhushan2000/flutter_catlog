870439323d15a4f6788505037639e72ef4b29ab0
DESKTOP <waghdharebhushan71@gmail.com>
2021-06-19 08:52:46 -0700

    day29 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,6 +3,8 @@ import 'dart:convert'; // it gives decoded json data
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_catlog/core/store.dart';
+import 'package:flutter_catlog/models/cart.dart';
 import 'package:flutter_catlog/utils/routes.dart';
 import 'package:flutter_catlog/widgets/home_widgets/catalog_header.dart';
 import 'package:flutter_catlog/widgets/home_widgets/catalog_list.dart';
@@ -12,6 +14,7 @@ import 'package:velocity_x/velocity_x.dart';
 import 'package:flutter_catlog/models/catlog.dart';
 
 import 'package:flutter_catlog/widgets/themes.dart';
+import 'package:http/http.dart' as http;
 
 // day 11 learn about context,constraints
 
@@ -25,6 +28,8 @@ class _HomePageState extends State<HomePage> {
 
   final String name = "Bhushan";
 
+  final url = "https://api.jsonbin.io/b/604dbddb683e7e079c4eefd3";
+
   // it initialize the _HomePageState class
   @override
   void initState() {
@@ -35,8 +40,14 @@ class _HomePageState extends State<HomePage> {
   loadData() async {
     await Future.delayed(Duration(seconds: 2));
 
-    final catalogJson =
-        await rootBundle.loadString("assets/files/catalog.json");
+    // retrive data from file
+    // final catalogJson =
+    //     await rootBundle.loadString("assets/files/catalog.json");
+
+    // retrive data from url
+    final response = await http.get(Uri.parse(url));
+    final catalogJson = response.body;
+
     final decodedData = jsonDecode(catalogJson);
     var productsdata = decodedData["products"];
     CatlaogModel.items =
@@ -46,17 +57,27 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final _cart = (VxState.store as MyStore).cart;
+
     return Scaffold(
       backgroundColor: context.canvasColor,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pushNamed(context, MyRoutes.cartRoute);
-        },
-        child: Icon(
-          CupertinoIcons.cart,
-          color: Colors.white,
-        ),
-        backgroundColor: context.theme.buttonColor,
+      floatingActionButton: VxBuilder(
+        mutations: {AddMutation, ReomveMutation},
+        builder: (ctx, s, _) => FloatingActionButton(
+          onPressed: () {
+            Navigator.pushNamed(context, MyRoutes.cartRoute);
+          },
+          child: Icon(
+            CupertinoIcons.cart,
+            color: Colors.white,
+          ),
+          backgroundColor: context.theme.buttonColor,
+        ).badge(
+            color: Vx.red500,
+            size: 24,
+            count: _cart!.items.length,
+            textStyle:
+                TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
       ),
       body: SafeArea(
         child: Container(
